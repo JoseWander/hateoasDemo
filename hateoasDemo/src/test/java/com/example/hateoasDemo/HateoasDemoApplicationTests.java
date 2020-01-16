e504b6c1ad2f842a275a4f49feafe73c9f71933f
JWNUNES <JWNUNES@NB35909S.corp.ad.emb>
2020-01-16 19:42:46 -0300

    Inicial

---
+++ hateoasDemo/src/test/java/com/example/hateoasDemo/HateoasDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hateoasDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateoasDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
